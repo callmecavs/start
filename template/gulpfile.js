cd50f7bc6f418d98d7519dd672b07c6fd288332d
Michael Cavalea <callmecavs@gmail.com>
2019-08-07 16:41:52 -0400

    first take at JS task

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -4,13 +4,18 @@ const {
 } = require('gulp')
 
 const autoprefixer = require('autoprefixer')
+const babel = require('rollup-plugin-babel')
+const commonjs = require('rollup-plugin-commonjs')
 const csso = require('gulp-csso')
 const htmlmin = require('gulp-htmlmin')
 const include = require('gulp-file-include')
 const postcss = require('gulp-postcss')
+const resolve = require('rollup-plugin-node-resolve')
+const rollup = require('rollup')
 const rucksack = require('rucksack-css')
 const sass = require('gulp-sass')
 const sourcemaps = require('gulp-sourcemaps')
+const { terser } = require('rollup-plugin-terser')
 
 // HTML
 
@@ -37,3 +42,31 @@ const css = () => src('src/sass/style.scss')
   .pipe(dest('dist'))
 
 exports.css = css
+
+// JS
+
+const read = {
+  input: 'src/js/main.js',
+  plugins: [
+    resolve({ browser: true }),
+    commonjs(),
+    babel({ exclude: 'node_modules/**' }),
+    terser()
+  ]
+}
+
+const write = {
+  file: 'dist/bundle.js',
+  format: 'iife',
+  output: {
+    name: 'bundle'
+  },
+  sourcemap: true
+}
+
+const js = async () => {
+  const bundle = await rollup.rollup(read)
+  await bundle.write(write)
+}
+
+exports.js = js
