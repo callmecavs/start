27fdd4cef08e4b51140f5162c600c9e31cee2490
Michael Cavalea <callmecavs@gmail.com>
2019-08-07 15:29:25 -0400

    add updated CSS task

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -3,8 +3,14 @@ const {
   src
 } = require('gulp')
 
+const autoprefixer = require('autoprefixer')
+const csso = require('gulp-csso')
 const htmlmin = require('gulp-htmlmin')
 const include = require('gulp-file-include')
+const postcss = require('gulp-postcss')
+const rucksack = require('rucksack-css')
+const sass = require('gulp-sass')
+const sourcemaps = require('gulp-sourcemaps')
 
 // HTML
 
@@ -15,4 +21,19 @@ const html = () => src('src/html/*.html')
 
 exports.html = html
 
-// Sass
+// CSS
+
+const processors = [
+  rucksack({ inputPseudo: false, quantityQueries: false }),
+  autoprefixer()
+]
+
+const css = () => src('src/sass/style.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  .pipe(postcss(processors))
+  .pipe(csso())
+  .pipe(sourcemaps.write('./maps'))
+  .pipe(dest('dist'))
+
+exports.css = css
